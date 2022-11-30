f941771c13593521fbde5d731b31a498e4448edb
dun <x.shi.2@umail.leidenuniv.nl>
2022-12-01 00:57:59 +0100

    Fixed environment variable

--- launch_app.sh
+++ launch_app.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-MASTER_NODE=node102 IS_MASTER=false java -jar build/libs/assn2-0.0.1-SNAPSHOT.jar
+MASTER_NODE=node102 IS_MASTER=false NODE_ID=node103 java -jar build/libs/assn2-0.0.1-SNAPSHOT.jar
+MASTER_NODE=node102 IS_MASTER=true NODE_ID=node102 java -jar build/libs/assn2-0.0.1-SNAPSHOT.jar
 
 # sed -i -e 's/\r$//' gradlew
