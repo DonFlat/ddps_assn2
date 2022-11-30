b051f4e53a8efa2321eb14dcf3c7f779eddb8d20
dun <x.shi.2@umail.leidenuniv.nl>
2022-11-30 22:58:57 +0100

    Added port number

--- src/main/kotlin/group6/assn2/service/MiniGfsClients.kt
+++ src/main/kotlin/group6/assn2/service/MiniGfsClients.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
 @Service
-@FeignClient(name = "miniGfsClient", url = "http://node102")
+@FeignClient(name = "miniGfsClient", url = "http://node102:2206")
 interface MiniGfsClients {
 
     @RequestMapping(method = [RequestMethod.GET], value = ["/"])
