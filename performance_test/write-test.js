cce5a4e2eaba1dfa4d436dfc04e16732ffbca9b9
dun <x.shi.2@umail.leidenuniv.nl>
2022-12-15 17:36:39 +0100

    Fix test script

--- performance_test/write-test.js
+++ performance_test/write-test.js
@@ -13,25 +13,25 @@ export default function () {
 
     // Write to chunkserver
     const content = 'This part sent from the k6 test'
-    if (chunkservers.length === 1) {
-        const writeFileUrl = `http://${chunkservers[0]}:2206/file/${fileName}/content/${3}`
+    if (chunkservers.body.length === 1) {
+        const writeFileUrl = `http://${chunkservers.body[0]}:2206/file/${fileName}/content/${3}`
         http.post(writeFileUrl, content)
     }
-    if (chunkservers.length === 2) {
-        const writeFileUrl0 = `http://${chunkservers[0]}:2206/file/${fileName}/content/${2}`
+    if (chunkservers.body.length === 2) {
+        const writeFileUrl0 = `http://${chunkservers.body[0]}:2206/file/${fileName}/content/${2}`
         http.post(writeFileUrl0, content)
 
-        const writeFileUrl1 = `http://${chunkservers[1]}:2206/file/${fileName}/content/${1}`
+        const writeFileUrl1 = `http://${chunkservers.body[1]}:2206/file/${fileName}/content/${1}`
         http.post(writeFileUrl1, content)
     }
-    if (chunkservers.length >= 3) {
-        const writeFileUrl0 = `http://${chunkservers[0]}:2206/file/${fileName}/content/${1}`
+    if (chunkservers.body.length >= 3) {
+        const writeFileUrl0 = `http://${chunkservers.body[0]}:2206/file/${fileName}/content/${1}`
         http.post(writeFileUrl0, content)
 
-        const writeFileUrl1 = `http://${chunkservers[1]}:2206/file/${fileName}/content/${1}`
+        const writeFileUrl1 = `http://${chunkservers.body[1]}:2206/file/${fileName}/content/${1}`
         http.post(writeFileUrl1, content)
 
-        const writeFileUrl2 = `http://${chunkservers[2]}:2206/file/${fileName}/content/${1}`
+        const writeFileUrl2 = `http://${chunkservers.body[2]}:2206/file/${fileName}/content/${1}`
         http.post(writeFileUrl2, content)
     }
 }
