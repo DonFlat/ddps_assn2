17185f556c853627b4334b3e3d2956641f0887d5
dun <x.shi.2@umail.leidenuniv.nl>
2022-12-15 01:24:50 +0100

    removed test client

--- scripts/test-client.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-fileName = "hello-world"
-api_url = f"http://localhost:8080/file/{fileName}/content"
-content = "This is hello world!"
-
-response = requests.post(api_url, data=content)
-
-print(response)
