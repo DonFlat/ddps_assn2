d668f439c5cc004493719448dd5c7e815000de2e
dun <x.shi.2@umail.leidenuniv.nl>
2022-12-15 17:52:50 +0100

    Fix test script

--- performance_test/write-test.js
+++ performance_test/write-test.js
@@ -3,35 +3,39 @@ import http from 'k6/http'
 export default function () {
 
     // Obtain leases
-    const masterNode = 'localhost'
+    const masterNode = 'node105'
     const fileName = 'hello-there'
 
     const getLeaseUrl = `http://${masterNode}:2206/${fileName}/lease`
 
     const chunkservers = http.get(getLeaseUrl)
     console.log(`chunkserver num: ${chunkservers.body.length}: ${chunkservers.body}`)
+    let servers = []
+    for (let i in chunkservers.body) {
+        servers.push(i)
+    }
 
     // Write to chunkserver
     const content = 'This part sent from the k6 test'
-    if (chunkservers.body.length === 1) {
-        const writeFileUrl = `http://${chunkservers.body[0]}:2206/file/${fileName}/content/${3}`
+    if (servers.length === 1) {
+        const writeFileUrl = `http://${servers[0]}:2206/file/${fileName}/content/${3}`
         http.post(writeFileUrl, content)
     }
-    if (chunkservers.body.length === 2) {
-        const writeFileUrl0 = `http://${chunkservers.body[0]}:2206/file/${fileName}/content/${2}`
+    if (servers.length === 2) {
+        const writeFileUrl0 = `http://${servers[0]}:2206/file/${fileName}/content/${2}`
         http.post(writeFileUrl0, content)
 
-        const writeFileUrl1 = `http://${chunkservers.body[1]}:2206/file/${fileName}/content/${1}`
+        const writeFileUrl1 = `http://${servers[1]}:2206/file/${fileName}/content/${1}`
         http.post(writeFileUrl1, content)
     }
-    if (chunkservers.body.length >= 3) {
-        const writeFileUrl0 = `http://${chunkservers.body[0]}:2206/file/${fileName}/content/${1}`
+    if (servers.length >= 3) {
+        const writeFileUrl0 = `http://${servers[0]}:2206/file/${fileName}/content/${1}`
         http.post(writeFileUrl0, content)
 
-        const writeFileUrl1 = `http://${chunkservers.body[1]}:2206/file/${fileName}/content/${1}`
+        const writeFileUrl1 = `http://${servers[1]}:2206/file/${fileName}/content/${1}`
         http.post(writeFileUrl1, content)
 
-        const writeFileUrl2 = `http://${chunkservers.body[2]}:2206/file/${fileName}/content/${1}`
+        const writeFileUrl2 = `http://${servers[2]}:2206/file/${fileName}/content/${1}`
         http.post(writeFileUrl2, content)
     }
 }
